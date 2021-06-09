f3a2f7ad4fc82219933c2e37db113d89d78b0346
llssdd <lewis.diamond@glasgow.ac.uk>
2021-06-09 21:43:06 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,21 @@ HC='\033[0;32m' # Heading Color
 WC='\033[0;33m' # Warning Color
 NC='\033[0m' # No Color
 
-PATH="/var/www/your_domain/"
+DIR="/var/www/your_domain/"
 FOLDER="helloWorld"
 GIT_URL="https://github.com/llssdd/helloWorld.git" # (i.e. https://cse031sust02@bitbucket.org/xyz.git)
 
 
-if [ -d "${PATH} ${FOLDER}" ]; then
+if [ -d "${DIR} ${FOLDER}" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${PATH} ${FOLDER}"
 else
   ###  Control will jump here if $DIR does NOT exists ###
-  echo "Error: ${PATH} ${FOLDER} not found. Can not continue."
-  mkdir ${PATH} ${FOLDER}
+  echo "Error: ${DIR} ${FOLDER} not found. Can not continue."
+  cd $DIR
+  mkdir $FOLDER
   exit 1
 fi
 echo -e "${HC}::::::::::::::::::::::::::Creating Project Directory::::::::::::::::::::::::::${NC}"
-cd "{PATH} ${FOLDER}"
+cd "{DIR} ${FOLDER}"
 echo -e "${HC}::::::::::::::::::::::::::In Project Directory::::::::::::::::::::::::::${NC}"
