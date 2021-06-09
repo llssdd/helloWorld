3cffdbabb2b6313faafadc4e069505d8978d3037
llssdd <lewis.diamond@glasgow.ac.uk>
2021-06-09 22:21:09 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-########################################################
-# Bash script to install HeavyGari Laravel App
-# Written by Talha Ibne Imam
-########################################################
-
-HC='\033[0;32m' # Heading Color
-WC='\033[0;33m' # Warning Color
-NC='\033[0m' # No Color
-
 DIR="/var/www/your_domain"
 FOLDER="helloWorld"
 GIT_URL="https://github.com/llssdd/helloWorld.git" # (i.e. https://cse031sust02@bitbucket.org/xyz.git)
 
 echo -e "${HC}::::::::::::::::::::::::::Creating Project Directory::::::::::::::::::::::::::${NC}"
+
 if [ -d "${DIR}/${FOLDER}" ]; then
   # Take action if $DIR exists. #
   echo "Dont need to create directory: ${DIR}/${FOLDER}"
@@ -24,9 +16,10 @@ else
   mkdir $FOLDER
   exit 1
 fi
+
 echo -e "${HC}::::::::::::::::::::::::::Project Directory Created::::::::::::::::::::::::::${NC}"
 cd "${DIR}/${FOLDER}"
-echo -e "We are in folder: $(pwd)"
+echo -e "${HC}We are in folder: $(pwd)${NC}"
 
 echo -e "${HC}::::::::::::::::::::::::::In Project Directory::::::::::::::::::::::::::${NC}"
 
